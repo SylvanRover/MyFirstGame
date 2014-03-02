4f7a0c3e4b8a3289e1c76eff42d9452c44497971
SylvanRover <contact@denmanrooke.com>
2014-03-02 14:09:40 +0000

    first commit

---
+++ Assets/EyeMovement.js
@@ -0,0 +1,149 @@
+﻿function Update () {
+
+            
+
+////////////////////
+
+    //mouse scrolling
+
+    
+
+    var mousePosX = Input.mousePosition.x;
+
+    var mousePosY = Input.mousePosition.y;
+
+    var scrollDistance : int = 5;
+
+    var scrollSpeed : float = 70;
+    
+ 
+
+	if (Input.GetMouseButton(0))
+		{
+	//Horizontal camera movement
+
+    if (mousePosX < scrollDistance)
+
+        //horizontal, left
+
+        { 
+
+        transform.Translate(-0.1, 0, 0);
+
+        } 
+
+    if (mousePosX >= Screen.width - scrollDistance)
+
+        //horizontal, right
+
+        { 
+
+        transform.Translate(0.1, 0, 0);
+
+        } 
+
+ 
+
+    //Vertical camera movement
+
+    if (mousePosY < scrollDistance)
+
+        //scrolling down
+
+        {
+
+        transform.Translate(0, -0.1, 0);
+
+        } 
+
+    if (mousePosY >= Screen.height - scrollDistance)
+
+        //scrolling up
+
+        {
+
+        transform.Translate(0, 0.1, 0);
+
+        }
+        
+        }
+
+    
+
+    ////////////////////
+
+    //zooming
+
+    var Eye : GameObject = GameObject.Find("Eye");
+
+    
+
+    //
+
+    if (Input.GetAxis("Mouse ScrollWheel") > 0 && Eye.camera.orthographicSize > 1)
+
+        {
+
+        Eye.camera.orthographicSize = Eye.camera.orthographicSize - 1;
+
+        }
+
+    
+
+    //
+
+    if (Input.GetAxis("Mouse ScrollWheel") < 0 && Eye.camera.orthographicSize < 5)
+
+        {
+
+        Eye.camera.orthographicSize = Eye.camera.orthographicSize + 1;
+
+        }
+
+ 
+
+    //default zoom
+
+    if (Input.GetKeyDown(KeyCode.Mouse2))
+
+        {
+
+        Eye.camera.orthographicSize = 50;
+
+        }
+        
+    if (transform.position.x < -9) 
+    
+    {
+	
+		transform.position.x = -9;
+
+	}
+	
+	if (transform.position.x > 9) 
+    
+    {
+	
+		transform.position.x = 9;
+
+	}
+	
+	if (transform.position.y < -9) 
+    
+    {
+	
+		transform.position.y = -9;
+
+	}
+	
+	if (transform.position.y > 9) 
+    
+    {
+	
+		transform.position.y = 9;
+
+	}
+
+        
+
+}
